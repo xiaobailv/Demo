19769a074285bf6132311b1b953823430cc10b02
liush <liushihaozs@foxmail.com>
2022-05-08 23:28:12 +0800

    main测试类

--- src/test/java/com/liushihao/junit/MainTest.java
+++ src/test/java/com/liushihao/junit/MainTest.java
@@ -1,40 +1,25 @@
 package com.liushihao.junit;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import org.junit.Test;
 
 public class MainTest {
 
     public static void main(String[] args) {
-        /*System.out.println(booleanTest());*/
-        /*try {
-            System.out.println(fileClose());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        Thread aa = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "::" + Thread.currentThread().isDaemon());
+            while (true) {
+
+            }
+        }, "aa");
+        // 设置守护线程
+        aa.setDaemon(true);
+        aa.start();
+
+        System.out.println(Thread.currentThread().getName() + " over");
     }
 
-    /*private static Boolean booleanTest() {
-        Integer a = 10;
-        return a == 2;
-    }*/
+    @Test
+    public void thread() {
 
-    private static String fileClose() throws IOException {
-        // 读文件
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("D:\\KL-Bank\\INFB-INFR\\INF20032801B"), StandardCharsets.UTF_8));
-        // 将读取到的字符串赋值给line
-        String line = br.readLine();
-        if (line != null) { // 如果读取到的不为null则说明文件有内容
-            br.close();
-            return line;
-        } else {
-            System.out.println("文件没有内容");
-        }
-        br.close();
-        System.out.println("结束T0206读取success文件...");
-        return null;
     }
 }
